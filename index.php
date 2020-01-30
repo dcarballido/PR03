dd2240b0fff01be43562552ea8ffbec3be9fc91c
AlexSalas <50219007+AlexSalas55@users.noreply.github.com>
2020-01-30 17:59:59 +0100

    Primera prueba de codigo

--- index.php
+++ index.php
@@ -9,10 +9,80 @@
 <div class="mycontacts">
 
     <div class="barra1">
-        <p>1213</p>
+        
+    <div class="logo-letra">
+        <a href="#">MyContacts</a>
+    </div>
+
+    <div class="todos-contactos">
+    <a href="#"><p>Todos los contactos</p></a>
+        
+    </div>
+
+
     </div>
     <div class="barra2">
-    <p>1213</p>
+
+    <div class="busqueda">
+
+        <div class="icono-busqueda">
+
+        </div>
+
+        <input type="text" id="buscar" autofocus placeholder="Buscar en contactos...">
+    </div>
+
+    <div class="contactos">
+
+        <table>
+
+            <tr>
+                    <td>Aapapa</td>
+            </tr>
+            <tr>
+                    <td>Aapapa</td>
+            </tr>
+            <tr>
+                    <td>Aapapa</td>
+            </tr>
+            <tr>
+                    <td>Aapapa</td>
+            </tr>
+            <tr>
+                    <td>Aapapa</td>
+            </tr>
+            <tr>
+                    <td>Aapapa</td>
+            </tr>
+            <tr>
+                    <td>Aapapa</td>
+            </tr>
+            <tr>
+                    <td>Aapapa</td>
+            </tr>
+            <tr>
+                    <td>Aapapa</td>
+            </tr>
+            <tr>
+                    <td>Aapapa</td>
+            </tr>
+            <tr>
+                    <td>Aapapa</td>
+            </tr>
+            <tr>
+                    <td>Aapapa</td>
+            </tr>
+            <tr>
+                    <td>Aapapa</td>
+            </tr>
+            <tr>
+                    <td>Aapapa</td>
+            </tr>
+
+        </table>
+
+    </div>
+
     </div>
     <div class="barra3">
     <p>1213</p>
